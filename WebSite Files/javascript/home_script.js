90a880dc54f693e759d4f10c805f2464557c22d3
Parabjot Chander <parabjotchander23@gmail.com>
2023-03-17 20:40:45 -0400

    homepage done, may make changes

--- WebSite Files/javascript/home_script.js
+++ WebSite Files/javascript/home_script.js
@@ -26,3 +26,31 @@ counters.forEach( counter => {
 
 
 })
+
+/* Udemy Idea #2 */
+
+var indexValue = 1;
+
+showImg(indexValue);
+
+function btm_slide(e){
+    showImg(indexValue = e);
+}
+
+function side_slide(e){
+  showImg(indexValue += e);
+}
+
+function showImg(e){
+  var i;
+  const img = document.querySelectorAll('.legend-photos img');
+  const sliders = document.querySelectorAll('.btm-sliders span');
+  if( e > img.length ){ indexValue =1; }
+  if( e < 1 ){ indexValue = img.length}
+  for( let i = 0; i < img.length; i++ ){
+    img[i].style.display = "none";
+  }
+
+  img[indexValue-1].style.display = "block";
+
+}
